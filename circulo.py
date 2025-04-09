3155be1c8ebba230d8bcd884fcad25dd0e6f70b4
YoshuaPariona <loganpariona.1207@gmail.com>
2025-04-09 11:42:13 -0500

    feat: implementar funcion para calcular area del circulo

--- circulo.py
+++ circulo.py
@@ -6,6 +6,10 @@ class Circulo:
     def circunferencia(self):
         return 2*self.PI*self.radio
 
+    def area(self):
+        return self.PI * self.radio * self.radio
+
+
 if __name__ == "__main__":
     inst_circulo = Circulo(10)
     print(f"La circunferencia es: {inst_circulo.circunferencia()}")
